529945c69c1665edbe13d1c6885fea3b79536323
Fatih Kaya <1994274@gmail.com>
2017-03-18 23:26:23 +0300

    plug node's promise api to mongoose

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const recordSchema = require('./record');
 
+// plug node's promise api to mongoose
+mongoose.Promise = Promise;
+
 mongoose.connect('mongodb://dbUser:dbPassword@ds155428.mlab.com:55428/getir-bitaksi-hackathon');
 
 exports.Record = mongoose.model('Record', recordSchema);
